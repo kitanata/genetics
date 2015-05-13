5841a98e855d61d086f50affbb7d38cd1955ab38
Raymond Chandler III <raymondchandleriii@gmail.com>
2015-05-13 02:03:20 -0400

    Better fitness functions and breeding.

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,18 @@ fn fitness(target: &Vec<char>, entity: &Vec<char>) -> i32 {
         }
     }
 
-    return 50 - fit_value;
+    return fit_value;
 }
 
 fn breed(mother: &Vec<char>, father: &Vec<char>) -> Vec<char> {
     let mut child = vec!['o';50];
 
-    for i in 0..50 {
-        if mother[i] == father[i] {
-            child[i] = 'o';
-        } else if mother[i] == 'X' || father[i] == 'X' {
-            child[i] = 'X';
-        }
+    for i in 0..25 {
+        child[i] = mother[i];
+    }
+
+    for i in 25..50 {
+        child[i] = father[i];
     }
 
     return child;
@@ -81,7 +81,14 @@ fn main() {
     while(run_generations) {
         generate_num += 1;
 
-        for j in 0..5 {
+        let gen_len = generation.len();
+        let num_pairs = gen_len / 2;
+
+        let mut pairs: Vec<(Vec<char>, Vec<char>)> = Vec::new();
+
+        println!("New Generation! Generating {} pairs.", num_pairs);
+
+        for j in 0..num_pairs {
             let mut rnd_index = 0;
             if generation.len() > 2 {
                 rnd_index = rng.gen_range(0, generation.len() - 2);
@@ -89,31 +96,70 @@ fn main() {
 
             let father = generation.remove(rnd_index);
             let mother = generation.remove(rnd_index);
-            let child_dna = mutate(&mut breed(&mother, &father));
 
-            println!("New Generation! {} Child #: {}", generate_num, j);
-            println!("Mother:  {}", dna_to_string(&mother));
-            println!("Father:  {}", dna_to_string(&father));
-            println!("Child:   {}", dna_to_string(&child_dna));
-            println!("Target:  {}", dna_to_string(&target));
+            pairs.push((father, mother));
+        }
 
-            if child_dna == target {
-                println!("SUCCESS! Generation #: {} Child #: {}", generate_num, j);
+        println!("Finished generating pair.");
+
+        for (mother, father) in pairs {
+            for k in 0..2 {
+                let child_dna = mutate(&mut breed(&mother, &father));
+
+                println!("Generation #: {} Child #: {}", generate_num, k);
+                println!("Mother:  {}", dna_to_string(&mother));
+                println!("Father:  {}", dna_to_string(&father));
                 println!("Child:   {}", dna_to_string(&child_dna));
                 println!("Target:  {}", dna_to_string(&target));
-                run_generations = false;
-                break;
-            } else {
+
+                if child_dna == target {
+                    println!("SUCCESS! Generation #: {} Child #: {}", generate_num, k);
+                    println!("Child:   {}", dna_to_string(&child_dna));
+                    println!("Target:  {}", dna_to_string(&target));
+                    run_generations = false;
+                    break;
+                }
+
                 generation.push(child_dna);
-                generation.push(father);
-                generation.push(mother);
+            }
+
+            generation.push(mother);
+            generation.push(father);
+
+            if run_generations == false {
+                break;
             }
         }
 
-        &generation[..].sort_by(|a, b| fitness(&target, &a).cmp(&fitness(&target, &b)));
+        let pool_size = generation.len();
+        println!("Current size of Gene Pool: {}!", pool_size);
 
-        for s in 0..5 {
-            generation.pop();
+        //Calulating mean fitness
+        let mut fit_mean = 0;
+        for s in &generation {
+            fit_mean += fitness(&target, s);
+        }
+        fit_mean = fit_mean / (pool_size as i32);
+        println!("Mean Fit: {}", fit_mean);
+
+        //Killing off the weak
+        let mut kill_count = 0;
+        let mut next_generation: Vec<Vec<char>> = Vec::new();
+        for s in &generation {
+            if fitness(&target, s) > fit_mean {
+                next_generation.push(s.clone());
+            } else {
+                kill_count += 1;
+            }
+        }
+
+        next_generation.push(generation.remove(0));
+
+        generation = next_generation;
+        println!("Next Generation size is {} entities!", generation.len());
+
+        if generation.len() < 2 {
+            panic!("Should not happen.");
         }
     }
 }
